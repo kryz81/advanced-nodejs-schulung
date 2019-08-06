2f0b697d2b878c4a5e4bb211561501267b62a83d
Kryz <kryz@webentwickler.io>
2019-08-06 17:54:18 +0200

    6: unpredictable function

---
+++ src/unpredictable-function/skills_reader.js
@@ -0,0 +1,43 @@
+const { readFile } = require('fs');
+
+const cache = {};
+
+const getSkills = (employee, cb) => {
+  // return from cache
+  if (cache[employee]) {
+    return cb(null, cache[employee]);
+  }
+
+  // if not in cache read employee data
+  readFile(`${__dirname}/data/${employee}.txt`, 'utf-8', (err, content) => {
+    if (err) {
+      return cb(err);
+    }
+
+    const skills = content.split(';').filter(String);
+
+    // cache employee skills
+    cache[employee] = skills;
+
+    cb(null, skills);
+  });
+};
+
+module.exports = employee => {
+  // multiple listeners can listen for employee skills
+  const listeners = [];
+
+  getSkills(employee, (err, result) => {
+    if (err) {
+      throw err;
+      return;
+    }
+
+    // send skills to each listener
+    listeners.forEach(listener => listener(result));
+  });
+
+  return {
+    onData: listener => listeners.push(listener),
+  };
+};
