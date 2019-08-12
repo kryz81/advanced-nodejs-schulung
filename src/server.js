2e60762424323c32d3890c55cdfc46f14318382d
Kryz <kryz@webentwickler.io>
2019-08-12 16:26:24 +0200

    debugging libs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+const debug = require('debug')('myapp');
 const { APP_PORT } = require('./config');
 const app = require('./app');
+
+debug('Server started');
 
 // eslint-disable-next-line no-console
 app.listen(APP_PORT, () => console.log(`Listening on ${APP_PORT}`));
