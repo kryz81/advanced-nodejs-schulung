729c643787a150c62e5c3081bbfd187d0c51eac8
Kryz <kryz@webentwickler.io>
2019-08-16 09:27:40 +0200

    http module

---
+++ src/http-module/app.js
@@ -0,0 +1,35 @@
+const { createServer } = require('http');
+
+const jobs = [{ id: '1', name: 'Job 1' }, { id: '2', name: 'Job 2' }];
+
+const router = (req, res) => {
+  const { pathname, searchParams } = new URL(req.url, 'http://localhost');
+  const path = pathname.substr(1);
+
+  let content = {};
+  let status = 200;
+  const headers = { 'Content-Type': 'application/json' };
+
+  switch (path) {
+    case 'jobs':
+      content = [...jobs];
+      break;
+    case 'job':
+      const foundJob = jobs.find(job => job.id === searchParams.get('id'));
+      if (foundJob) {
+        content = foundJob;
+      } else {
+        status = 404;
+      }
+      break;
+    default:
+      status = 404;
+  }
+
+  res.writeHead(status, headers);
+  res.end(JSON.stringify(content));
+};
+
+const app = () => createServer(router);
+
+module.exports = app;
---
+++ src/http-module/server.js
@@ -0,0 +1,4 @@
+const app = require('./app');
+
+// eslint-disable-next-line
+app().listen(3000, () => console.log('Listening...'));
