9533d726838a5337058b28de3b3f3c9eda853930
kryz <k@kryz.dev>
2019-08-26 16:17:05 +0200

    8 preinitialization queue (#9)
    * 8: preinitialization queue
    
    * 8: switch db/poll

---
+++ src/preinitialization_queue/index.js
@@ -0,0 +1,27 @@
+const { createServer } = require('http');
+const { connect, getUser } = require(__dirname + '/db');
+// const { connect, getUser } = require(__dirname + '/pool');
+
+connect(
+  'mongo://localhost',
+  (err, result) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`DB connection: ${result}`);
+  }
+);
+
+const server = createServer((req, res) => {
+  getUser(req.url.substring(1), (err, result) => {
+    if (err) {
+      res.writeHead(500);
+      res.end(err.message);
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+  });
+});
+
+server.listen(3000);
