b23bc0f37310925d016ec057c9e48c08c9ac3235
Kryz <kryz@webentwickler.io>
2019-08-26 16:58:23 +0200

    refactoring, update deps

--- src/services/__tests__/buildResponse.test.js
+++
@@ -1,9 +0,0 @@
-const buildResponse = require('../buildResponse');
-
-test('creates correct response', () => {
-  expect(buildResponse('/home')).toBe('[home]');
-});
-
-test('throws error on empty url', () => {
-  expect(() => buildResponse()).toThrow('Invalid url');
-});
