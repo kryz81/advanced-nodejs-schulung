91d2494cfba76b2f618b47d277c2b3a640fdbdfa
kryz <k@kryz.dev>
2019-08-26 16:16:16 +0200

    6: unpredictable function (#7)

---
+++ src/unpredictable-function/index.js
@@ -0,0 +1,13 @@
+const skillsReader = require(__dirname + '/skills_reader');
+
+const reader = skillsReader('user1');
+
+reader.onData(skills => {
+  console.log(skills);
+
+  const reader2 = skillsReader('user1');
+
+  reader2.onData(skills => {
+    console.log(skills);
+  });
+});
